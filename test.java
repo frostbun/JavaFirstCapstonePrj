9913af03fb314a50ea616b7c8c7609ed42a88c3f
Nguyen Manh <manhnb1911@gmail.com>
2020-12-31 22:07:44 +0700

    all

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class test {
  
@@ -29,79 +28,16 @@ public class test {
         }
         return ret;
     }
-
-    private Rater getRaterById(ArrayList<Rater> data, String id) {
-        for(Rater r: data) {
-            if(r.getID().equals(id)) {
-                return r;
-            }
-        }
-        return null;
-    }
-    
-    private int getRatingByMovie(ArrayList<Rater> data, String id) {
-        int count = 0;
-        for(Rater r: data) {
-            if(r.hasRating(id)) {
-                ++count;
-            }
-        }
-        return count;
-    }
-    
-    private int getMovieByRater(ArrayList<Rater> data) {
-        HashSet<String> uniqueMovies = new HashSet<>();
-        for(Rater r: data) {
-            for(String movie: r.getItemsRated()) {
-                uniqueMovies.add(movie);
-            }
-        }
-        return uniqueMovies.size();
-    }
     
     public static void main(String[] args){
         System.out.println("Testing");
+
+        test _main = new test();
         FirstRatings fr = new FirstRatings();
         MovieRunnerAverage mra = new MovieRunnerAverage();
-        test _main = new test();
 
         mra.printAverageRatings(12);
         // mra.getAverageRatingOneMovie("Vacation");
 
-        // ArrayList<Movie> data = fr.loadMovies("data/ratedmoviesfull.csv");
-        // data = _main.filterMovieBy(data, new GenresFilter(new String[]{"Comedy", }));
-        // data = _main.filterMovieBy(data, new LengthFilter(151, 9999999));
-        // System.out.println(data.size());
-        // for(Movie m: data) {
-        //     System.out.println(m);
-        // }
-
-        // HashMap<String, ArrayList<Movie>> directors = _main.parseDataToDirectors(data);
-        // String directorOfMaxMovie = "";
-        // int maxMovie = 0;
-        // for(String curDirector: directors.keySet()){
-        //     // System.out.println(curDirector + "\t" + directors.get(curDirector).size());
-        //     if(directors.get(curDirector).size() > maxMovie) {
-        //         maxMovie = directors.get(curDirector).size();
-        //         directorOfMaxMovie = curDirector;
-        //     }
-        // }
-        // System.out.println(directorOfMaxMovie + "\t" + maxMovie);
-
-        // ArrayList<Rater> data = fr.loadRaters("data/ratings.csv");
-        // String raterWithMaxRating = "";
-        // int maxRating = 0;
-        // for(Rater r: data) {
-        //     // System.out.println(r.getID() + "\t" + r.numRatings());
-        //     if(r.numRatings() > maxRating) {
-        //         maxRating = r.numRatings();
-        //         raterWithMaxRating = r.getID();
-        //     }
-        // }
-        // System.out.println(raterWithMaxRating + "\t" + maxRating);
-
-        // System.out.println(_main.getRaterById(data, "193").numRatings());
-        // System.out.println(_main.getRatingByMovie(data, "1798709"));
-        // System.out.println(_main.getMovieByRater(data));
     }
 }
