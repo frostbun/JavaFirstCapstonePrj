4d20406cef4a195601d065aa50417db074746940
Nguyen Manh <manhnb1911@gmail.com>
2021-01-10 16:35:26 +0700

    w3

--- test.java
+++ test.java
@@ -1,23 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+// import java.util.ArrayList;
+// import java.util.HashMap;
 
 public class test {
     
-    private HashMap<String, ArrayList<Movie>> parseDataToDirectors(ArrayList<Movie> data) {
-        HashMap<String, ArrayList<Movie>> ret = new HashMap<>();
-        for(Movie m: data) {
-            String[] directors = m.getDirector().split(", ");
-            for(String curDirector: directors){
-                ArrayList<Movie> movies = new ArrayList<>();
-                if(ret.keySet().contains(curDirector)) {
-                    movies = ret.get(curDirector);
-                }
-                movies.add(m);
-                ret.put(curDirector, movies);
-            }
-        }
-        return ret;
-    }
+    // private HashMap<String, ArrayList<Movie>> parseDataToDirectors(ArrayList<Movie> data) {
+    //     HashMap<String, ArrayList<Movie>> ret = new HashMap<>();
+    //     for(Movie m: data) {
+    //         String[] directors = m.getDirector().split(", ");
+    //         for(String curDirector: directors){
+    //             ArrayList<Movie> movies = new ArrayList<>();
+    //             if(ret.keySet().contains(curDirector)) {
+    //                 movies = ret.get(curDirector);
+    //             }
+    //             movies.add(m);
+    //             ret.put(curDirector, movies);
+    //         }
+    //     }
+    //     return ret;
+    // }
     
     public static void main(String[] args){
         System.out.println("Testing");
